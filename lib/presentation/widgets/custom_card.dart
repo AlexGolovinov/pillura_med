46e431ce7a23e9d737adb7655b9def8ff390f992
AlexGolovinov <41750183+AlexGolovinov@users.noreply.github.com>
2025-09-19 18:39:00 +0500

    Update custom_card.dart

--- lib/presentation/widgets/custom_card.dart
+++ lib/presentation/widgets/custom_card.dart
@@ -3,21 +3,25 @@ import 'package:flutter/material.dart';
 Widget customCard({required String title, required bool isSelected}) {
   return SizedBox(
     height: 41,
-    child: IntrinsicWidth(
-      child: Card(
-        color: isSelected ? Color(0xFF4459D4) : Colors.white,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12),
-          child: Center(
-            child: Text(
+    child: Card(
+      color: isSelected ? const Color(0xFF4459D4) : Colors.white,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        child: Row(
+          mainAxisSize: MainAxisSize.min, // ширина под содержимое
+          children: [
+            Text(
               title,
+              maxLines: 1,
+              softWrap: false,
+              overflow: TextOverflow.visible,
               style: TextStyle(
                 color: isSelected ? Colors.white : Colors.black,
                 fontWeight: isSelected ? FontWeight.w600 : FontWeight.normal,
               ),
             ),
-          ),
+          ],
         ),
       ),
     ),
